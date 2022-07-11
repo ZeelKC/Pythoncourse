da5aa413153a24b395549b8f05c5b3f28ede8d21
ZeelKC <107108317+ZeelKC@users.noreply.github.com>
2022-07-11 14:16:43 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -2,29 +2,31 @@
 name=input("Enter filename: ")
 ex=name.split('.')
 ex=ex[1]
+d={'doc':'Word document','html':'html file','pdf':'pdf','ppt':'Power point presenation','xls':'excel sheet','txt':'text file' ,
+     'py':'python file','c':'c file','jpg':'image','zip':'zip file','mvk':'video file','mp3':'audio file'}
 if ex=='doc' or ex=='docx':
-    print('Word document')
+    print(d['doc'])
 elif ex=='html' or ex=='htm':
-    print('html file')
+    print(d['html'])
 elif ex=='pdf':
-    print('pdf')
+    print(d['pdf'])
 elif ex=='ppt' or ex=='pptx':
-    print('Power point presenation')
+    print(d['ppt'])
 elif ex=='xls' or ex=='xlsx':
-    print("excel sheet")
+    print(d['xls'])
 elif ex=='txt':
-    print('text file')
+    print(d['txt'])
 elif ex=='py':
-    print('python file')
+    print(d['py'])
 elif ex=='c':
-    print('c file')
+    print(d['c'])
 elif ex=='jpg' or ex=='png':
-    print('image')
+    print(d['jpg'])
 elif ex=='zip':
-    print("zip file")
+    print(d['zip'])
 elif ex=='mvk' or ex=='mp4':
-    print('video file')
+    print(d['mvk'])
 elif ex=='mp3':
-    print('audio file')
+    print(d['mp3'])
 else:
     print('extenstion unrecognized')
