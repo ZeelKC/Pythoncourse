0e91904267ee3da36ef478257ad41e4a6db68b48
ZeelKC <107108317+ZeelKC@users.noreply.github.com>
2022-07-20 21:09:51 +0530

    Update frequency.py

--- frequency.py
+++ frequency.py
@@ -2,6 +2,7 @@
 '''ex:  Mississippi
 m=1 ; i=4 ; s=4 ; p=2
 '''
+import operator
 def most_frequent():
 
   w=input("Enter the word or sentence:\n")
@@ -11,9 +12,11 @@ def most_frequent():
     if i in d:
         d[i] = d[i] + 1
     else:
-        d[i] = 1
-
-   
+        d[i] = 1 
   return d
+
 f=most_frequent()
-print(f)
+s_f = dict( sorted(f.items(), key=operator.itemgetter(1),reverse=True))
+print(s_f)
+
+
